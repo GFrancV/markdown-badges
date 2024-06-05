9b6436111945fe9740aabe1b0832d14bb8832424
GFrancV <gfrancv@hotmail.com>
2024-06-05 12:12:50 +0100

    chore: Add site-map plugin to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import vue from "@astrojs/vue"
 
-import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
-});
+	site: "https://markdown-badges.vercel.app",
+	integrations: [tailwind(), vue(), sitemap()],
+})
