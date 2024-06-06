3142a0f5b71f214c8d7fa2a73c58ca31d9e2625f
GFrancV <gfrancv@hotmail.com>
2024-06-06 14:32:23 +0100

    build: Add vercel adapter to deploy site in astro with web analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import vue from "@astrojs/vue"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import vue from "@astrojs/vue";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from "@astrojs/sitemap"
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://markdown-badges.vercel.app",
 	integrations: [tailwind(), vue(), sitemap()],
-})
+	output: "server",
+	adapter: vercel({
+		webAnalytics: { enabled: true },
+	}),
+});
