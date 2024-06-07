d01e8e6b00a4fbdc9cdf4f6253afede29e87a078
GFrancV <gfrancv@hotmail.com>
2024-06-07 11:49:36 +0100

    feat: Add mousetrap library for keyboard shortcut functionality

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-	import { ref, type Ref } from "vue"
+	import { onMounted, onUnmounted, ref, type Ref } from "vue"
+	import Mousetrap from "mousetrap"
 	import badges from "../consts/badges.json"
 	import type { Badge } from "../env"
 
@@ -44,6 +45,23 @@
 				'<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="inherit"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></svg>'
 		}, 1000)
 	}
+
+	onMounted(() => {
+		Mousetrap.bind("ctrl+k", (e: Event) => {
+			if (e.preventDefault) {
+				e.preventDefault()
+			}
+
+			const searchInput = document.querySelector('input[name="query"]') as HTMLInputElement
+			if (searchInput) {
+				searchInput.focus()
+			}
+		})
+	})
+
+	onUnmounted(() => {
+		Mousetrap.unbind("ctrl+k")
+	})
 </script>
 
 <template>
@@ -68,13 +86,32 @@
 		</div>
 
 		<input
-			type="search"
+			type="text"
 			name="query"
 			v-model="query"
 			class="block ps-12 bg-[#1e1e1e] rounded w-full border-0 text-[#f1f1ef] focus:ring focus:ring-fuchsia-200 placeholder:text-neutral-600 mb-10 shadow-lg"
 			@input="searchBadget"
-			placeholder="Search for a badget"
+			:placeholder="`Search in ${badges.length} badges`"
 		/>
+
+		<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-gray-600">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="stroke-gray-600"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M7 9a2 2 0 1 1 2 -2v10a2 2 0 1 1 -2 -2h10a2 2 0 1 1 -2 2v-10a2 2 0 1 1 2 2h-10" />
+			</svg>
+			<span class="text-[17px]"> K </span>
+		</div>
 	</div>
 	<div v-if="results" class="grid md:grid-cols-4 grid-cols-2 gap-4">
 		<div
@@ -84,7 +121,12 @@
 			@click="copy(badge.markdown, $event)"
 		>
 			<h2 class="text-xl font-semibold text-[#f1f1ef] mb-3">{{ badge.name }}</h2>
-			<img :src="badge.url" :alt="badge.name" class="mt-auto aspect-[4/1] w-3/4 mx-auto" loading="lazy" />
+			<img
+				:src="badge.url"
+				:alt="badge.name"
+				class="mt-auto aspect-[4/1] w-3/4 mx-auto mb-4"
+				loading="lazy"
+			/>
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="24"
