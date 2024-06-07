b7a653cf2f511325403a499ae45b5896dfaa7e89
GFrancV <gfrancv@hotmail.com>
2024-06-07 16:00:17 +0100

    feat: Add cross for clear search in search input

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -114,23 +114,26 @@
 			:placeholder="`Search in ${badges.length} badges`"
 		/>
 
-		<div class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-gray-600">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				width="24"
-				height="24"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-				stroke-width="2"
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				class="stroke-gray-600"
-			>
-				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-				<path d="M7 9a2 2 0 1 1 2 -2v10a2 2 0 1 1 -2 -2h10a2 2 0 1 1 -2 2v-10a2 2 0 1 1 2 2h-10" />
-			</svg>
-			<span class="text-[17px]"> K </span>
+		<div class="absolute inset-y-0 right-0 flex items-center pr-3">
+			<span v-if="!query && query.length <= 0" class="flex items-center pointer-events-none text-gray-600">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="stroke-gray-600"
+				>
+					<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+					<path d="M7 9a2 2 0 1 1 2 -2v10a2 2 0 1 1 -2 -2h10a2 2 0 1 1 -2 2v-10a2 2 0 1 1 2 2h-10" />
+				</svg>
+				<span class="text-[17px]"> K </span>
+			</span>
+			<button v-else @click="clearSearch" class="text-white">x</button>
 		</div>
 	</div>
 	<div v-if="results && results.length > 0" class="grid md:grid-cols-4 grid-cols-2 gap-4">
