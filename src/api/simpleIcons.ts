fca5ae7ec8a9d3e4bb02bb72f59abea3dad4f396
GFrancV <gfrancv@hotmail.com>
2024-11-25 10:56:36 +0000

    feat: Add SimpleIcon interface and fetch function for dynamic icon retrieval

---
+++ src/api/simpleIcons.ts
@@ -0,0 +1,25 @@
+export interface SimpleIcon {
+  title: string;
+  hex: string;
+  source: string;
+  aliases?: Aliases;
+  license?: License;
+  guidelines?: string;
+}
+
+interface Aliases {
+  aka: string[];
+}
+
+interface License {
+  type: string;
+}
+
+export const getIcons = async (): Promise<SimpleIcon[]> => {
+  const req = await fetch(
+    "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/_data/simple-icons.json"
+  );
+  const data = await req.json();
+
+  return data.icons;
+};
