4ecea42bad67a48440484c43bb0abf24b6e6564f
GFrancV <gfrancv@hotmail.com>
2024-11-26 09:59:19 +0000

    feat: Enhance BadgeGenerator with clipboard functionality and dynamic icon updates

--- src/utils/BadgeGenerator.ts
+++ src/utils/BadgeGenerator.ts
@@ -1,5 +1,10 @@
 import { $ } from "./dom-selector";
 
+const clipboardIcon =
+  '<svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></svg>';
+const copyClipboardIcon =
+  '<svg  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 14l2 2l4 -4" /></svg>';
+
 export class BadgeGenerator extends HTMLElement {
   private name: string = "GitHub";
   private logocolor: string = "ffffff";
@@ -59,10 +64,18 @@ export class BadgeGenerator extends HTMLElement {
       this.icon = newValue;
     }
     this.img.src = this.getSrc();
+    this.updateCode();
   }
 
   public getSrc(): string {
     return `https://img.shields.io/badge/${this.name}-1000?style=for-the-badge&logo=${this.icon}&logoColor=${this.logocolor}&labelColor=${this.leftcolor}&color=${this.rightcolor}`;
+  }
+
+  private updateCode() {
+    this.markdownCode.textContent = `![${this.name}](${this.getSrc()})`;
+    this.imgCode.textContent = `<img src="${this.getSrc()}" alt="${
+      this.name
+    }" />`;
   }
 
   private createCopyButton(code: string): HTMLButtonElement {
@@ -74,12 +87,18 @@ export class BadgeGenerator extends HTMLElement {
       "transition",
       "stroke-neutral-300",
       "opacity-0",
-      "group-hover:opacity-100"
+      "group-hover:opacity-100",
+      "hover:stroke-fuchsia-300"
     );
-    copyButton.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="inherit" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <path stroke="none" d="M0 0h24v24H0z" fill="none"></path> <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path> <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path></svg>';
+    copyButton.innerHTML = clipboardIcon;
+
     copyButton.addEventListener("click", () => {
       navigator.clipboard.writeText(code);
+
+      copyButton.innerHTML = copyClipboardIcon;
+      setTimeout(() => {
+        copyButton.innerHTML = clipboardIcon;
+      }, 1000);
     });
 
     return copyButton;
