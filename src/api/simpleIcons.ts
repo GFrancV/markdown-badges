66852cf1326de83007ebff6a7daa22057656304d
GFrancV <gfrancv@hotmail.com>
2025-03-24 11:26:46 +0000

    fix(api): Update getIcons function to return complete data from API

--- src/api/simpleIcons.ts
+++ src/api/simpleIcons.ts
@@ -18,8 +18,8 @@ interface License {
 export const getIcons = async (): Promise<SimpleIcon[]> => {
   const req = await fetch(
     "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/_data/simple-icons.json"
-  );
+  );  
   const data = await req.json();
 
-  return data.icons;
+  return data;
 };
