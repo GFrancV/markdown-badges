a6ba1bac06f1931336b19ff84ec7971892492192
GFrancV <35277540+GFrancV@users.noreply.github.com>
2025-08-11 12:30:01 +0100

    fix: Fix simple icons api url

--- src/api/simpleIcons.ts
+++ src/api/simpleIcons.ts
@@ -17,7 +17,7 @@ interface License {
 
 export const getIcons = async (): Promise<SimpleIcon[]> => {
   const req = await fetch(
-    "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/_data/simple-icons.json"
+    "https://raw.githubusercontent.com/simple-icons/simple-icons/develop/data/simple-icons.json"
   );  
   const data = await req.json();
 
