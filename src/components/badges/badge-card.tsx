fd678fe5843d24ae63226a8a25c6f67a5acf13df
GFrancV <gfrancv@hotmail.com>
2026-03-05 15:31:46 +0000

    feat(ui): update BadgeCard component styles and integrate Button for category display

--- src/components/badges/badge-card.tsx
+++ src/components/badges/badge-card.tsx
@@ -1,6 +1,7 @@
 import { ClipboardCheckIcon, ClipboardIcon } from "lucide-react";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Button } from "../ui/button";
 
 export function BadgeCard({ badge }: { badge: Badge }) {
   const { name, url, category } = badge;
@@ -21,7 +22,7 @@ export function BadgeCard({ badge }: { badge: Badge }) {
   return (
     <div
       onClick={handleCopy}
-      className="relative group bg-[#1e1e1e] cursor-pointer text-white rounded-sm border border-transparent transition p-6 text-center flex flex-col hover:bg-fuchsia-300/30 hover:border-fuchsia-200 badget-element h-full"
+      className="relative group bg-[#1e1e1e] cursor-pointer text-white rounded-sm border border-transparent transition p-6 text-center flex flex-col hover:bg-primary/20 hover:border-primary badget-element h-full"
     >
       <h2 className="text-[#f1f1ef] text-lg font-semibold mt-1 mb-3">{name}</h2>
       <img
@@ -33,11 +34,14 @@ export function BadgeCard({ badge }: { badge: Badge }) {
         loading="lazy"
       />
       <div className="mt-2">
-        <div className="rounded-full border border-neutral-300 px-2 py-1 text-sm text-neutral-300 group-hover:border-fuchsia-300 group-hover:text-fuchsia-300 transition duration-300 w-auto">
+        <Button
+          variant="outline"
+          className="rounded-full group-hover:border-primary group-hover:text-primary"
+        >
           {category}
-        </div>
+        </Button>
       </div>
-      <button className="absolute top-0 right-0 m-2 text-gray-600 transition duration-300 group-hover:text-fuchsia-300 text-xl">
+      <button className="absolute top-0 right-0 m-2 text-gray-600 transition duration-300 group-hover:text-primary text-xl">
         {!isCopied ? (
           <ClipboardIcon size={22} />
         ) : (
