dc67cedc1ec8a44681dc19c5d0f556f8da419b62
GFrancV <gfrancv@hotmail.com>
2026-03-18 16:17:30 +0000

    feat: refactor BadgesList component to accept badges as props and create badges index page

--- src/components/badges/badges-list.tsx
+++ src/components/badges/badges-list.tsx
@@ -1,17 +1,14 @@
-import { getBadges } from "@/services/badges";
-import { BadgeCard } from "./badge-card";
-import { BadgeSidebarProvider } from "./badge-sidebar";
+import { BadgeCard } from "@/components/badges/badge-card";
+import { ClientProviders } from "@/components/client-providers";
 
-export function BadgesList() {
-  const badges = getBadges();
-
+export function BadgesList({ badges }: { badges: Badge[] }) {
   return (
-    <BadgeSidebarProvider>
+    <ClientProviders>
       <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
         {badges.map((badge) => (
           <BadgeCard key={badge.id} badge={badge} />
         ))}
       </div>
-    </BadgeSidebarProvider>
+    </ClientProviders>
   );
 }
