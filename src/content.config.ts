1376e1b9d7b73c43ff9e9bdb5948adeb7fea9d15
GFrancV <gfrancv@hotmail.com>
2026-05-04 16:52:46 +0100

    fix(badge): update file paths for badge data and adjust TypeScript configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,7 +3,7 @@ import { z } from "astro/zod";
 import { defineCollection } from "astro:content";
 
 const badges = defineCollection({
-  loader: file("src/data/badges.json"),
+  loader: file("data/badges.json"),
   schema: z.object({
     id: z.string(),
     name: z.string(),
